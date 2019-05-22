6e28326f77a872e11bbd6e97d46d6785a8eef2aa
hg398 <rockingharsh398@gmail.com>
2019-05-22 20:49:58 +0530

    added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
 	console.log("New user connected.");
 
+	socket.emit('newMessage', {
+		from: 'ankit',
+		text: 'see you!',
+		createAt: 123
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log(message);
+	});
+
 	socket.on('disconnect', () => {
 		console.log("DIsconnected!");
 	});
