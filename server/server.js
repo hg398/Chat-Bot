c7e51f94cb55b4c88eec5eb5a4850ba24b154077
hg398 <rockingharsh398@gmail.com>
2019-05-22 20:05:39 +0530

    Added connected and disconnected event handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const express=require('express'), 
-	  app=express(),
  	  path=require('path'),
- 	  port=process.env.PORT || 3000;
+ 	  http=require('http'),
+ 	  socketIO=require('socket.io'),
+ 	  port=process.env.PORT || 3000,
 	  publicPath=path.join(__dirname,'../public');
+
+var app=express(),
+	server=http.createServer(app),
+	io=socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket) => {
+	console.log("New user connected.");
+
+	socket.on('disconnect', () => {
+		console.log("DIsconnected!");
+	});
+})
 // app.get("/",function(req,res){
 // 	res.render("index");
 // })
 
-app.listen(port,function(){
+server.listen(port,() => {
 	console.log("Running on port 3000!");
-})
+});
